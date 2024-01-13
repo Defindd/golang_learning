b1d4031d87acced8349859b058dcccfa762d9082
Defindd <prilukov_nikita@mail.ru>
2024-01-13 17:07:08 +0400

    functions

--- firstproj.go
+++ firstproj.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+//var name string = "Nikita"
 
 func main() {
+	/* fmt.Printf("My name is %s\n", name)
+
 	fmt.Println("Hello world!")
 	fmt.Print("Hello world2!")
 	fmt.Println("Hello world3!")
+	//var hello string = "hello world 4"
+	b1 := true
+	//var hello2 string = "qwerty"
+	//fmt.Printf("Type: %T Value:%v\n", hello, hello)
+	fmt.Printf("%+d, %o %t, ", -5, 175, b1)
+	fmt.Print("123")
+	fmt.Println("c\u00e1t")
+	var a rune = 128512
+	println(string(a))  */
+	frm, wlc := SayHi("Nikita", "Prilukov")
+	fmt.Printf("%s,%s", frm, wlc)
+}
+func SayHi(firstname, secondname string) (formatedString string, welcome string) {
+	formatedString = fmt.Sprintf("Hi %q", firstname+" "+secondname)
+	welcome = "welcome to c1dy's ass"
+	return
 }
