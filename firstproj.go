259df7faa15f37023cf05ea20f6d543db4bf0aa6
Defindd <prilukov_nikita@mail.ru>
2024-02-19 01:22:16 +0400

    slices

--- firstproj.go
+++ firstproj.go
@@ -5,8 +5,11 @@ import (
 )
 
 func main() {
-	var weekTemp = [7]int{5, 4, 6, 8, 11, 9, 5}
-	var qwerty = &weekTemp
-	qwerty[2] = 123
-	fmt.Println(weekTemp, *qwerty)
+	a := []int{1, 2, 3}
+	fmt.Printf("%p \n", a)
+	b := a[1:2]
+	b = append(b, 15)
+	fmt.Printf("%p\n", b)
+	b = append(b, 75)
+	fmt.Println(a, b)
 }
